e948b5ddab684dd4935f621802720663d4575a44
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-02-26 20:32:28 -1000

    making the stuff break

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,6 +1,7 @@
 $(function(){
   console.log("hello");
   create_grid();
+  create_colorPicker();
 
   $('.square').click(function(){
     console.log("square color");
@@ -8,6 +9,9 @@ $(function(){
   });
 });
 
+
+//PIXEL GRID
+function create_grid (){
 var row = 20;
 var column = 20;
 var $row = $("<div>", {
@@ -16,8 +20,6 @@ var $row = $("<div>", {
 var $square = $("<div>",{
   class: 'square'
 });
-
-function create_grid (){
   for(var i = 0; i < column; i++){
     $row.append($square.clone());
   }
@@ -28,17 +30,35 @@ function create_grid (){
 }
 
 
-// COLOR PICKER
-$(".highlights .columns .highlight").click(function () {
-  // console.log('hello', $(this));
-  // random number between 0x000000 and 0xFFFFFF
-  var random_r = Math.floor( Math.random()*256);
-  var random_g = Math.floor( Math.random()*256);
-  var random_b = Math.floor( Math.random()*256);
+// COLOR PICKER GRID
+function create_colorPicker (){
+var row = 20;
+var column = 10;
+var $row = $("<div>", {
+  class : 'row'
+});
+var $square = $("<div>",{
+  class: 'square'
+});
+  for(var i = 0; i < column; i++){
+    $row.append($square.clone());
+  }
 
-  $(this).css({
-    "background" : "rgb("+ random_r +","+ random_g +","+ random_b +")"
-  });
+  for(var j = 0; j < row; j++){
+    $("#controls").append($row.clone());
+  }
+}
+// COLOR PICKER
+// $(".highlights .columns .highlight").click(function () {
+//   // console.log('hello', $(this));
+//   // random number between 0x000000 and 0xFFFFFF
+//   var random_r = Math.floor( Math.random()*256);
+//   var random_g = Math.floor( Math.random()*256);
+//   var random_b = Math.floor( Math.random()*256);
+
+//   $(this).css({
+//     "background" : "rgb("+ random_r +","+ random_g +","+ random_b +")"
+//   });
 
 // function PixelPainter (width, height){
 //   this.artboard = $
